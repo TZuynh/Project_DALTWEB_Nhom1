99aead5bdc37a7599736027f3cebeb9af388a93a
baolam224 <151661123+baolam224@users.noreply.github.com>
2024-11-30 01:58:39 +0700

    api product với productdetail
    route trong api.php

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,28 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\User\Api\APILoginController;
 /*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
+|----------------------------------------------------------------------|
+| API Routes                                                           |
+|----------------------------------------------------------------------|
+| Routes cho các API                                                   |
+|----------------------------------------------------------------------|
 */
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+// Các route không yêu cầu xác thực (ví dụ đăng nhập, đăng ký người dùng)
+// Route đăng nhập API
+Route::get('/', function () {
+    return redirect('/login');
 });
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    
+    
+    // Các route khác chỉ người dùng đăng nhập mới có thể truy cập (ví dụ: đổi mật khẩu, thông tin tài khoản, v.v.)
+    //Route::get('/user', [App\Http\Controllers\User\Api\APIUserController::class, 'getUserInfo']);  // Route lấy thông tin người dùng
+});
+Route::get('/products', [App\Http\Controllers\User\Api\APIProductController::class, 'index']);
+
+Route::get('/productdetail', [App\Http\Controllers\User\Api\APIProductDetailController::class, 'index']);
